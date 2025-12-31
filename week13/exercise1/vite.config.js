c30dd82393ec9e12d019f68966b77250ff091dc4
treinartz <thomas.reinartz@gmail.com>
2025-12-31 15:57:29 -0600

    Update base path in Vite configuration

--- week13/exercise1/vite.config.js
+++ week13/exercise1/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_assignments2/week13/exercise13/',
+  base: '/react_assignments2/week13/exercise1/',
 
 })
